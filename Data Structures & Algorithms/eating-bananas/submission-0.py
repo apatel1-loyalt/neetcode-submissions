49b3faf8dc825a6169b972bf1c710cf3c8d30515
Ankur Patel <93269161+apatel1-loyalt@users.noreply.github.com>
2026-04-04 17:31:41 -0600

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # Eating Rate
+        k_min = 1
+        k_max = 0
+
+        
+
+        # find maximum amount by finding the largest pile of banana
+        for pile in piles:
+            if k_max < pile:
+                k_max = pile
+
+        k = k_max
+
+        # Now that we have our search area lets find the minimum eating rate 
+        while k_min <= k_max:
+            m = (k_min + k_max) // 2
+
+            time = 0
+            for pile in piles:
+                time += math.ceil(pile/m)
+
+            if time > h:
+                k_min = m + 1
+            elif time <= h:
+                k = min(k,m)
+                k_max = m - 1
+
+        return k
